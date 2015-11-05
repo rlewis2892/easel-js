687d0518033e20a90760ad9e4bbc0e0d290e3e1b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-11-05 12:01:23 -0700

    change circle to a star

--- js/easel.js
+++ js/easel.js
@@ -21,13 +21,13 @@ function init() {
 	// Let's create a shape. First, we need a new Shape instance.
 	// Then we can use shape's graphics API to give it color and bounds.
 	// Position it on the canvas with x and y coordinates, and add it to our stage.
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0,0,50);
+	var star = new createjs.Shape();
+	star.graphics.beginFill("#FF00EE").drawPolyStar(0, 0, 50, 5, 0.6, -90);
 
 	// draw the circle at coordinates that are 50% of the canvas width, and height (i.e., the "center" :P)
-	circle.x = stageWidth * 0.5;
-	circle.y = stageHeight * 0.5;
-	stage.addChild(circle);
+	star.x = stageWidth * 0.5;
+	star.y = stageHeight * 0.5;
+	stage.addChild(star);
 
 	// Update our stage to display the shape
 	stage.update();
