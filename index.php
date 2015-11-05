e9729b05bbd3573b13d19f67e2cfebb6be999f67
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-11-05 11:42:47 -0700

    clean up HTML head

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=Edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- Latest compiled and minified CSS -->
+		<!-- Bootstrap latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-		<!-- Optional theme -->
+		<!-- Optional Bootstrap Theme -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
 		<!-- Custom CSS -->
@@ -21,19 +21,16 @@
 			<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!-- jQuery (required for Bootstap's JS plugins) -->
+		<!-- jQuery -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
-		<!-- Latest compiled and minified JavaScript -->
+		<!-- Bootstrap latest compiled and minified JavaScript -->
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
-		<!-- Custom JavaScript Utility Functions-->
-		<script src="js/utils.js" type="text/javascript"></script>
-
-		<!-- EaselJS-->
+		<!-- EaselJS -->
 		<script src="//code.createjs.com/easeljs-0.8.1.min.js"></script>
 
-		<!-- Easel JS Animation-->
+		<!-- Custom JS -->
 		<script src="js/utils.js" type="text/javascript"></script>
 		<script src="js/easel.js" type="text/javascript"></script>
 
